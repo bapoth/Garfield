1a182aa3282fb6a5a47af31c0e9e13cb8f402a51
Florian <flol2204+github@googlemail.com>
2017-03-02 23:33:39 +0100

    changed not working i2c core - the signals will be routed outside the core and the tri-state pin must be instantiated in the   module top of this core - changed tcl file to generate pins automaticly
    -> new Garfield Design following tomorrow

--- FPGA_Design/ip_extern/i2c/i2c_opencores/i2c_opencores.v
+++ FPGA_Design/ip_extern/i2c/i2c_opencores/i2c_opencores.v
@@ -8,7 +8,7 @@ module i2c_opencores
 (
 	wb_clk_i, wb_rst_i, wb_adr_i, wb_dat_i, wb_dat_o,
 	wb_we_i, wb_stb_i, /*wb_cyc_i,*/ wb_ack_o, wb_inta_o,
-	scl_pad_io, sda_pad_io
+	sda_pad_in, sda_pad_out , sda_pad_en, scl_pad_in, scl_pad_out, scl_pad_en
 );
 
 
@@ -27,27 +27,30 @@ output       wb_ack_o;		// WISHBONE acknowledge output
 output       wb_inta_o; 	// WISHBONE interrupt output
 
 // I2C signals
-inout        scl_pad_io;	// I2C clock io
-inout        sda_pad_io;	// I2C data io
+input sda_pad_in;
+input scl_pad_in;
+output sda_pad_out;
+output scl_pad_out;
+output sda_pad_en;
+output scl_pad_en;
 
 wire        wb_cyc_i;		// WISHBONE cycle input
 // Wire tri-state scl/sda
 wire scl_pad_i;
 wire scl_pad_o;
-wire scl_pad_io;
 wire scl_padoen_o;
-
-assign wb_cyc_i = wb_stb_i;
-assign scl_pad_i = scl_pad_io;
-assign scl_pad_io = scl_padoen_o ? 1'bZ : scl_pad_o;
-
 wire sda_pad_i;
 wire sda_pad_o;
-wire sda_pad_io;
 wire sda_padoen_o;
 
-assign sda_pad_i = sda_pad_io;
-assign sda_pad_io = sda_padoen_o ? 1'bZ : sda_pad_o;
+assign wb_cyc_i = wb_stb_i;
+
+assign scl_pad_i = scl_pad_in;
+assign sda_pad_i = sda_pad_in;
+assign sda_pad_out = sda_pad_o;
+assign scl_pad_out = scl_pad_o;
+assign sda_pad_en = sda_padoen_o;
+assign scl_pad_en = scl_padoen_o;
 
 // Avalon doesn't have an asynchronous reset
 //  set it to be inactive and just use synchronous reset
@@ -60,11 +63,11 @@ assign arst_i = 1'b1;
 i2c_master_top i2c_master_top_inst
 (
 	.wb_clk_i(wb_clk_i), .wb_rst_i(wb_rst_i), .arst_i(arst_i),
-	
+
 	.wb_adr_i(wb_adr_i), .wb_dat_i(wb_dat_i), .wb_dat_o(wb_dat_o),
 	.wb_we_i(wb_we_i), .wb_stb_i(wb_stb_i), .wb_cyc_i(wb_cyc_i),
 	.wb_ack_o(wb_ack_o), .wb_inta_o(wb_inta_o),
-	
+
 	.scl_pad_i(scl_pad_i), .scl_pad_o(scl_pad_o), .scl_padoen_o(scl_padoen_o),
 	.sda_pad_i(sda_pad_i), .sda_pad_o(sda_pad_o), .sda_padoen_o(sda_padoen_o)
 );
