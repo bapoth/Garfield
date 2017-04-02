2cd6ec55cb44ee68345c4c2c42399a8dd25fdb0c
Florian <flol2204+github@googlemail.com>
2017-04-02 14:03:57 +0200

    Finales FPGA image hinzugefügt inkl. finalen output folder

---
+++ FPGA_Design/output_files/01042017_final/script/flash_programmer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# This file was automatically generated.
+#
+# It can be overwritten by nios2-flash-programmer-generate or nios2-flash-programmer-gui.
+#
+
+#
+# Converting SOF File: /home/florian/Schreibtisch/Studium/Master_Informatik/HSP/ALF.git/FPGA_Design/output_files/01042017_final/Garfield.sof to: "../flash/Garfield_epcs_flash_controller_0.flash"
+#
+sof2flash --input="/home/florian/Schreibtisch/Studium/Master_Informatik/HSP/ALF.git/FPGA_Design/output_files/01042017_final/Garfield.sof" --output="../flash/Garfield_epcs_flash_controller_0.flash" --epcs --verbose 
+
