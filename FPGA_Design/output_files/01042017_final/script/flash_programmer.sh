0730d4a39f297e4acda7c3e8c8262915b2f6600e
Florian <flol2204+github@googlemail.com>
2017-04-02 18:19:42 +0200

    dateien um auf den epcs alles draufzuballern

--- FPGA_Design/output_files/01042017_final/script/flash_programmer.sh
+++ FPGA_Design/output_files/01042017_final/script/flash_programmer.sh
@@ -6,7 +6,7 @@
 #
 
 #
-# Converting SOF File: /home/florian/Schreibtisch/Studium/Master_Informatik/HSP/ALF.git/FPGA_Design/output_files/01042017_final/Garfield.sof to: "../flash/Garfield_epcs_flash_controller_0.flash"
+# Converting ELF File: /home/florian/Schreibtisch/Studium/Master_Informatik/HSP/ALF.git/FPGA_Design/output_files/01042017_final/test_flash.elf to: "../flash/test_flash_epcs_flash_controller_0.flash"
 #
-sof2flash --input="/home/florian/Schreibtisch/Studium/Master_Informatik/HSP/ALF.git/FPGA_Design/output_files/01042017_final/Garfield.sof" --output="../flash/Garfield_epcs_flash_controller_0.flash" --epcs --verbose 
+elf2flash --input="/home/florian/Schreibtisch/Studium/Master_Informatik/HSP/ALF.git/FPGA_Design/output_files/01042017_final/test_flash.elf" --output="../flash/test_flash_epcs_flash_controller_0.flash" --epcs --verbose --after="/home/florian/Schreibtisch/Studium/Master_Informatik/HSP/ALF.git/FPGA_Design/output_files/01042017_final/flash/Garfield_epcs_flash_controller_0.flash" 
 
