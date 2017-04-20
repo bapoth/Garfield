b6bc9d61062aa2313c63f0777927de7dd600b649
Florian <flol2204+github@googlemail.com>
2017-04-20 21:44:00 +0200

    finale Hardwareversion

---
+++ FPGA_Design/output_files/01042017_finalfinal/script/flash_programmer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# This file was automatically generated.
+#
+# It can be overwritten by nios2-flash-programmer-generate or nios2-flash-programmer-gui.
+#
+
+#
+# Programming File: "../flash/Garfield_epcs.flash_controller_0.flash" To Device: epcs_flash_controller_0
+#
+nios2-flash-programmer "../flash/Garfield_epcs.flash_controller_0.flash" --base=0xa0000 --epcs --sidp=0x10000 --id=0x1042017 --timestamp=1491915468 --device=2 --instance=0 '--cable=DE-SoC on localhost [2-1]' --program 
+
